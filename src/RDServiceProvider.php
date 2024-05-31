4fefbce919ea91862d5335f228fba0aea5f93b4b
Jorzel <jorzel@beedelivery.com.br>
2024-05-31 17:47:16 -0300

    one more

--- src/RDServiceProvider.php
+++ src/RDServiceProvider.php
@@ -7,6 +7,21 @@ use Illuminate\Support\ServiceProvider;
 
 class RDServiceProvider extends ServiceProvider
 {
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__ . '/../config/rd.php', 'rd');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('rd', function () {
+            return new RD;
+        });
+    }
+
     /**
      * Bootstrap the application services.
      */
@@ -22,23 +37,8 @@ class RDServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/rd.php' => config_path('rd.php'),
+                __DIR__ . '/../config/rd.php' => config_path('rd.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-rd'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-rd'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-rd'),
-            ], 'lang');*/
 
             // Registering package commands.
             $this->commands([
@@ -48,16 +48,12 @@ class RDServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register the application services.
-     */
-    public function register()
-    {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/rd.php', 'rd');
-
-        // Register the main class to use with the facade
-        $this->app->singleton('rd', function () {
-            return new RD;
-        });
-    }
+    * Get the services provided by the provider.
+    *
+    * @return array
+    */
+   public function provides()
+   {
+       return ['rd'];
+   }
 }
